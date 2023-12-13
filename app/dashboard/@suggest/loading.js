d99a77001cc602db5dfdc2436d56efa74e845482
dihanto <dihanto2306@gmail.com>
2023-12-13 19:20:41 +0700

    ✨(suggest) :  add loading ui

---
+++ app/dashboard/@suggest/loading.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="w-1/5 max-h-12 mt-5">
+      <div className="w-10/12">
+        <div className="h-9 bg-gray-300 rounded-lg my-1"></div>
+      </div>
+      <div className="w-10/12 mt-9">
+        <div className="h-9 bg-gray-300 rounded-lg"></div>
+      </div>
+      <div className="w-10/12 mt-2">
+        <div className="h-9 bg-gray-300 rounded-lg"></div>
+      </div>
+      <div className="w-10/12 mt-2">
+        <div className="h-9 bg-gray-300 rounded-lg"></div>
+      </div>
+      <div className="w-10/12 mt-2">
+        <div className="h-9 bg-gray-300 rounded-lg"></div>
+      </div>
+      <div className="w-10/12 mt-2">
+        <div className="h-9 bg-gray-300 rounded-lg"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
