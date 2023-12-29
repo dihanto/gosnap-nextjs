27826d242ff2ee73ac58ed84c9bd727ce3867b8b
dihanto <dihanto2306@gmail.com>
2023-12-30 01:26:39 +0700

    💄 update the ui to make the edit and the option button dissapear when user click outside the form

--- components/option/edit_photo.jsx
+++ components/option/edit_photo.jsx
@@ -3,16 +3,37 @@ import { FetchApiWithBody } from "../libs/api-libs";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function EditPhoto({ photoId, onUpdate, onEdit }) {
   const { data: session } = useSession();
   const [caption, setCaption] = useState("");
   const [submited, setSubmited] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(true);
+  const editFormRef = useRef(null);
 
   const handleChange = (e) => {
     if (e.target.name === "caption") {
       setCaption(e.target.value);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (e) => {
+    if (
+      editFormRef.current &&
+      !editFormRef.current.contains(e.target) &&
+      e.target.tagName !== "BUTTON"
+    ) {
+      onEdit();
+      setShowEditForm(false);
     }
   };
 
@@ -33,19 +54,23 @@ export default function EditPhoto({ photoId, onUpdate, onEdit }) {
   if (!submited) {
     return (
       <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="caption"
-            name="caption"
-            value={caption}
-            onChange={handleChange}
-            className={`rounded-md focus:outline-none ${
-              submited ? "hidden" : ""
-            }`}
-          ></input>
-          <button type="submit">Save Change</button>
-        </form>
+        {showEditForm && (
+          <div ref={editFormRef}>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="caption"
+                name="caption"
+                value={caption}
+                onChange={handleChange}
+                className={`rounded-md focus:outline-none ${
+                  submited ? "hidden" : ""
+                }`}
+              ></input>
+              <button type="submit">Save Change</button>
+            </form>
+          </div>
+        )}
       </div>
     );
   } else {
